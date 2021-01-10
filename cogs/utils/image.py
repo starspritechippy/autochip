f4412dd095a030f0287309c49a3c077878aea312
Maximilian Heymann <maxhey4@gmail.com>
2021-01-10 21:46:16 +0100

    improve ascii command output

--- cogs/utils/image.py
+++ cogs/utils/image.py
@@ -3,70 +3,70 @@ from io import BytesIO
 from PIL import Image
 
 ASCII_CHARS = [
-    ' ',
-    "$",
-    "@",
-    "B",
-    "%",
-    "8",
-    "&",
-    "W",
-    "M",
-    "#",
-    "*",
-    "a",
-    "h",
-    "k",
-    "b",
-    "d",
-    "p",
-    "q",
-    "w",
-    "m",
-    "O",
-    "0",
-    "Q",
-    "L",
-    "J",
-    "X",
-    "z",
-    "v",
-    "u",
-    "n",
-    "r",
-    "j",
-    "f",
-    "t",
-    "/",
-    "\\",
-    "|",
-    "(",
-    ")",
-    "1",
-    "{",
-    "}",
-    "[",
-    "]",
-    "?",
-    "-",
-    "_",
-    "+",
-    "~",
-    "<",
-    ">",
-    "i",
-    "!",
-    "l",
-    "I",
-    ";",
-    ":",
-    ",",
-    '"',
-    "^",
-    "`",
-    "'",
-    ".",
     " ",
+    ".",
+    "'",
+    "´",
+    "^",
+    '"',
+    ",",
+    ":",
+    ";",
+    "I",
+    "l",
+    "!",
+    "i",
+    ">",
+    "<",
+    "~",
+    "+",
+    "_",
+    "-",
+    "?",
+    "]",
+    "[",
+    "}",
+    "{",
+    "1",
+    ")",
+    "(",
+    "|",
+    "\\",
+    "/",
+    "t",
+    "f",
+    "j",
+    "r",
+    "n",
+    "u",
+    "v",
+    "z",
+    "X",
+    "J",
+    "L",
+    "Q",
+    "0",
+    "O",
+    "m",
+    "w",
+    "q",
+    "p",
+    "d",
+    "b",
+    "k",
+    "h",
+    "a",
+    "*",
+    "#",
+    "M",
+    "W",
+    "&",
+    "8",
+    "%",
+    "B",
+    "@",
+    "$",
+    "#",
 ]
 
 
@@ -79,15 +79,6 @@ async def get_bytes(bot, url):
 def open_image(res):
     img = Image.open(BytesIO(res))
     return img
-
-
-def change_contrast(img, level):
-    factor = (259 * (level + 255)) / (255 * (259 - level))
-
-    def contrast(c):
-        return 128 + factor * (c - 128)
-
-    return img.point(contrast)
 
 
 def resize_image(img: Image, width=57):
@@ -105,13 +96,17 @@ def gray_image(img: Image):
 
 def pixels_to_ascii(img: Image):
     pixels = img.getdata()
-    characters = "".join([ASCII_CHARS[pixel // 20] for pixel in pixels])
+    characters = "".join([ASCII_CHARS[pixel // 4] for pixel in pixels])
     return characters
 
 
 def image_to_ascii(img_bytes):
     pixels = pixels_to_ascii(
-        gray_image(resize_image(change_contrast(open_image(img_bytes), 250)))
+        gray_image(
+            resize_image(
+                open_image(img_bytes)
+            )
+        )
     )
     pixel_count = len(pixels)
     done = "\n".join(pixels[i : i + 57] for i in range(0, pixel_count, 57))
