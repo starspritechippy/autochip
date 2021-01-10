cb273fc1bbb64d11e9fd828b0a2e5a0342508c48
Maximilian Heymann <maxhey4@gmail.com>
2021-01-10 19:38:49 +0100

    optimize ascii generation

--- cogs/utils/image.py
+++ cogs/utils/image.py
@@ -2,7 +2,67 @@ from io import BytesIO
 
 from PIL import Image
 
-ASCII_CHARS = ['"', '$', '@', 'B', '%', '8', '&', 'W', 'M', '#', '*', 'a', 'h', 'k', 'b', 'd', 'p', 'q', 'w', 'm', 'O', '0', 'Q', 'L', 'J', 'X', 'z', 'v', 'u', 'n', 'r', 'j', 'f', 't', '/', '\\', '|', '(', ')', '1', '{', '}', '[', ']', '?', '-', '_', '+', '~', '<', '>', 'i', '!', 'l', 'I', ';', ':', ',', '"', '^', '`', "'", '.', ' ']
+ASCII_CHARS = [
+    '"',
+    "$",
+    "@",
+    "B",
+    "%",
+    "8",
+    "&",
+    "W",
+    "M",
+    "#",
+    "*",
+    "a",
+    "h",
+    "k",
+    "b",
+    "d",
+    "p",
+    "q",
+    "w",
+    "m",
+    "O",
+    "0",
+    "Q",
+    "L",
+    "J",
+    "X",
+    "z",
+    "v",
+    "u",
+    "n",
+    "r",
+    "j",
+    "f",
+    "t",
+    "/",
+    "\\",
+    "|",
+    "(",
+    ")",
+    "1",
+    "{",
+    "}",
+    "[",
+    "]",
+    "?",
+    "-",
+    "_",
+    "+",
+    "~",
+    "<",
+    ">",
+    "i",
+    "!",
+    "l",
+    "I",
+    ";",
+    "'",
+    ".",
+    " ",
+]
 
 
 async def get_bytes(bot, url):
@@ -21,13 +81,14 @@ def change_contrast(img, level):
 
     def contrast(c):
         return 128 + factor * (c - 128)
+
     return img.point(contrast)
 
 
 def resize_image(img: Image, width=57):
     orig_width, orig_height = img.size
     ratio = orig_height / orig_width
-    height = int((width * ratio)/2)
+    height = int((width * ratio) / 2)
     resized_image = img.resize((width, height))
     return resized_image
 
@@ -39,20 +100,14 @@ def gray_image(img: Image):
 
 def pixels_to_ascii(img: Image):
     pixels = img.getdata()
-    characters = "".join([ASCII_CHARS[pixel//20] for pixel in pixels])
+    characters = "".join([ASCII_CHARS[pixel // 20] for pixel in pixels])
     return characters
 
 
 def image_to_ascii(img_bytes):
     pixels = pixels_to_ascii(
-        gray_image(
-            resize_image(
-                change_contrast(
-                    open_image(img_bytes), 250
-                )
-            )
-        )
+        gray_image(resize_image(change_contrast(open_image(img_bytes), 250)))
     )
     pixel_count = len(pixels)
-    done = "\n".join(pixels[i:i + 57] for i in range(0, pixel_count, 57))
+    done = "\n".join(pixels[i : i + 57] for i in range(0, pixel_count, 57))
     return done
